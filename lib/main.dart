9e24115d11e46b61cb0da1200678328fff88cc8a
Italo Vinicios <italovinicios@gmail.com>
2019-11-27 20:21:40 -0300

    lab com step 2 concluido (stateless widget)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask me Anything!'),
+      ),
+      body: Container(
+       
+        color: Colors.blue.shade300,
+        child: Center(child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Image.asset('images/ball1.png'),
+        )),
+      ),
+    );
+  }
+}
